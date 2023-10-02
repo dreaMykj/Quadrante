39b9c31dc8ff4d34ba64baff57baf593baccc2e4
dreaMykj <gabrielgreghi2018@gmail.com>
2023-10-02 11:45:31 -0300

    Concluido

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿double x, y;
+string local;
+
+Console.WriteLine("--- Quadrante Cartesiano ---");
+
+Console.WriteLine("Entre com o ponto desejado.");
+
+Console.Write("Cordenada x: ");
+x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Cordenada y: ");
+y = Convert.ToInt32(Console.ReadLine());
+
+if (x == 0 && y ==0 ) local = "a origem";
+else if (x == 0) local = "o eixo das ordenadas";
+else if (y == 0) local = "o eixo das abcissas";
+else if (y > 0)
+{
+    if (x > 0)
+        local = "o quadrante 1";
+    else    
+        local = "o quadrante 2";
+    
+}
+else 
+{
+    if (x < 0)
+        local = "o quadrante 3";
+    else
+        local = "o quadrante 4";
+}
+
+Console.WriteLine($"O ponto ({x},{y}) fica n{local}");
